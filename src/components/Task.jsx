aa8d4636d523c48bf26adece816bc2b1c23a5e8b
Matteo <m2lovric@gmail.com>
2020-11-27 02:50:04 +0100

    Update Task component to use state

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-const Task = ({ task, dueDate }) => {
+const Task = () => {
+  const tasks = useSelector(state => state);
+  console.log(tasks)
   return (
-    <article className="list__task">
-      <p className="list__task__task">{task}</p>
-      <p className="list__task__dueDate">{dueDate}</p>
-    </article>
+    <section>
+      {
+        tasks.map((el) => {
+          return (
+            <article className="list__task" key={el.id}>
+              <p className="list__task__task">{el.task}</p>
+              <p className="list__task__dueDate">{el.dueDate}</p>
+              <button className={el.done ? 'green list__task__done' : 'red list__task__done'}>{el.done}</button>
+              <button className="list__task__remove">Remove</button>
+            </article>
+          )
+        })
+      }
+    </section>
   )
 }
 
