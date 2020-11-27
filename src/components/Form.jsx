76e4cb9243a6d9023b6a55cfbf1f87599ce22dea
Matteo <m2lovric@gmail.com>
2020-11-27 02:49:10 +0100

    Update form component with addTask method

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { newTask } from '../redux/actions'
 
 const Form = () => {
+  const dispacth = useDispatch();
+
+  const addTask = (e) => {
+    e.preventDefault();
+    const task = e.target.task.value;
+    const dueDate = e.target.duedate.value;
+    e.target.task.value = '';
+    e.target.duedate.value = '';
+    dispacth(newTask(task, dueDate))
+  }
   return (
-    <form className="form">
+    <form className="form" onSubmit={addTask}>
       <input type="text" name="task" placeholder="Enter a task :" id="" className="form__input" />
       <input type="datetime-local" name="duedate" id="" className="form__input" />
       <button type="submit">+</button>
