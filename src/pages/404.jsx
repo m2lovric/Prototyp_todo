8f2b7b24a287fe3f64e47c1f0c5d250f19c6cfa1
Matteo <m2lovric@gmail.com>
2020-11-27 02:50:26 +0100

    Add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from '../components/Layout'
+
+import '../scss/app.scss'
+
+export default function Home() {
+  return (
+    <Layout>
+      <p>404: Page not found</p>
+    </Layout>
+  )
+}
