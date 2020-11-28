eacb111f34ca888c65de41d8c5613c3d8dfd68d6
Matteo <m2lovric@gmail.com>
2020-11-28 01:57:13 +0100

    Add remove case to reducer

--- src/redux/taskReducer.js
+++ src/redux/taskReducer.js
@@ -2,6 +2,8 @@ export const taskReducer = (state, action) => {
   switch (action.type) {
     case 'ADD':
       return [...state, action.data]
+    case 'REMOVE':
+      return state = action.data
     default:
       return state
   }
