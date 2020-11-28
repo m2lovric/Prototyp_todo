335dc807e5927eef5b4c47d41cfe72fd94fac2bc
Matteo <m2lovric@gmail.com>
2020-11-28 01:56:43 +0100

    Add removeTask method to Task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTaskAction } from '../redux/actions'
 
 const Task = () => {
   const tasks = useSelector(state => state);
   console.log(tasks)
+  const dispacth = useDispatch();
+  tasks.map(el => console.log(el.id))
+
+  const removeTask = (e) => {
+    const id = e.target.parentNode.getAttribute('data-key');
+    const modState = tasks.filter((task) => {
+      return id !== task.id
+    });
+    dispacth(removeTaskAction(modState))
+  }
+
   return (
     <section>
       {
         tasks.map((el) => {
           return (
-            <article className="list__task" key={el.id}>
+            <article className="list__task" key={el.id} data-key={el.id}>
               <p className="list__task__task">{el.task}</p>
               <p className="list__task__dueDate">{el.dueDate}</p>
               <button className={el.done ? 'green list__task__done' : 'red list__task__done'}>{el.done}</button>
-              <button className="list__task__remove">Remove</button>
+              <button className="list__task__remove" onClick={removeTask}>Remove</button>
             </article>
           )
         })
