f0ce648976396d0885000aa2e900b2a47aa526ae
Matteo <m2lovric@gmail.com>
2020-12-01 00:38:48 +0100

    Set state for inputs and fire SignUp method

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,25 @@ import { Link } from 'gatsby'
 import React, { useState } from 'react'
 import Modal from 'react-modal'
 
+import { signUp } from '../firebase/auth'
+
 const Header = () => {
   const [showLogIn, setShowLogIn] = useState(false);
   const [showSignIn, setShowSignIn] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
 
   const handleCloseLog = () => setShowLogIn(false);
   const handleShowLog = () => setShowLogIn(true);
   const handleCloseSign = () => setShowSignIn(false);
   const handleShowSign = () => setShowSignIn(true);
+
+  const signUpForm = (e) => {
+    e.preventDefault();
+    signUp(email, password, passwordConfirm);
+  }
 
   return (
     <header>
@@ -24,11 +35,32 @@ const Header = () => {
       <Modal overlayClassName="modal__popup__overlay" className="modal__popup" isOpen={showLogIn}>
         <p>Log In</p>
         <button onClick={handleCloseLog}>Close</button>
+        <form action="">
+          <label htmlFor="">Email</label>
+          <input type="text" />
+          <label htmlFor="">Password</label>
+          <input type="password" name="" id="" />
+          <button type="submit">Log In</button>
+        </form>
       </Modal>
 
-      <Modal overlayClassName="modal__popup__overlay" className="modal__popup" isOpen={showSignIn}>
-        <p>Sign In</p>
-        <button onClick={handleCloseSign}>Close</button>
+      <Modal overlayClassName="modal__popup__overlay" className="modal__popup signup" isOpen={showSignIn}>
+        <section>
+          <p>Sign Up</p>
+          <button onClick={handleCloseSign}>Close</button>
+        </section>
+        <form className="signup__form" onSubmit={signUpForm}>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="signup_email" onChange={(e) => setEmail(e.target.value)} />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="signup_password" onChange={(e) => setPassword(e.target.value)} />
+
+          <label htmlFor="password_confirm">Confirm Password</label>
+          <input type="password" name="password_confirm" id="signup_password_confirm" onChange={(e) => setPasswordConfirm(e.target.value)} />
+
+          <button className="signup__btn" type="submit">Sign Up</button>
+        </form>
       </Modal>
     </header>
   )
