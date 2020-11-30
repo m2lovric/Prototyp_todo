65e65493c04499d30d84197555794218c72c7525
Matteo <m2lovric@gmail.com>
2020-12-01 00:39:00 +0100

    Init firebase app

---
+++ src/firebase/app.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaTY1xW5DtOW2HgVTguhbT89neAwR2008",
+  authDomain: "prototyp-todo.firebaseapp.com",
+  databaseURL: "https://prototyp-todo.firebaseio.com",
+  projectId: "prototyp-todo",
+  storageBucket: "prototyp-todo.appspot.com",
+  messagingSenderId: "983772207364",
+  appId: "1:983772207364:web:42b494e7f03354e1811851",
+  measurementId: "G-HWEJSDRLPX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
