34212760a108d4dcabedf2b4ad51a23558c8c741
Matteo <m2lovric@gmail.com>
2020-12-02 22:45:57 +0100

    Add object interface, change types, fix typo

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,31 +1,39 @@
+import { SetFieldsOnGraphQLNodeTypeArgs } from 'gatsby';
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { removeTaskAction } from '../redux/actions'
 
 const Task = () => {
-  const tasks = useSelector(state => state);
+  interface ObjectTask {
+    id: string,
+    task: string,
+    dueDate: Date,
+    done: boolean
+  }
+
+  const tasks = useSelector((state: []) => state);
   console.log(tasks)
-  const dispacth = useDispatch();
-  tasks.map((el: object) => console.log(el.id))
+  const dispatch = useDispatch();
+  tasks.map((el: ObjectTask) => console.log(el.id))
 
   const removeTask = (e: any) => {
-    const id = e.target.parentNode.getAttribute('data-key');
-    const modState = tasks.filter((task: object) => {
+    const id = e.target.dataset.id;
+    const modState = tasks.filter((task: ObjectTask) => {
       return id !== task.id
     });
-    dispacth(removeTaskAction(modState))
+    dispatch(removeTaskAction(modState))
   }
 
   return (
     <section>
       {
-        tasks.map((el) => {
+        tasks.map((el: ObjectTask) => {
           return (
-            <article className="list__task" key={el.id} data-key={el.id}>
+            <article className="list__task" key={el.id} >
               <p className="list__task__task">{el.task}</p>
               <p className="list__task__dueDate">{el.dueDate}</p>
               <button className={el.done ? 'green list__task__done' : 'red list__task__done'}>{el.done}</button>
-              <button className="list__task__remove" onClick={removeTask}>Remove</button>
+              <button className="list__task__remove" data-id={el.id} onClick={removeTask}>Remove</button>
             </article>
           )
         })
