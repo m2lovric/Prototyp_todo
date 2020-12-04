f8228eabbf0be168d990628662fc55cd89c70f65
Matteo <m2lovric@gmail.com>
2020-12-04 22:57:40 +0100

    Update classes

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import Layout from '../components/Layout';
 
 const SignIn = () => {
   return (
-    <>
-      <p>Sign In</p>
-      <form action="">
-        <label htmlFor="">Email</label>
-        <input type="text" />
-        <label htmlFor="">Password</label>
-        <input type="password" name="" id="" />
-        <button type="submit">Sign In</button>
-      </form>
-    </>
+    <Layout>
+      <section className="sign">
+        <article>
+          <p>Sign In</p>
+        </article>
+        <form action="" className="sign__form">
+          <label htmlFor="">Email</label>
+          <input type="text" />
+          <label htmlFor="">Password</label>
+          <input type="password" name="" id="" />
+          <button type="submit" className="sign__btn red">Sign In</button>
+        </form>
+      </section>
+    </Layout>
   )
 }
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import { signUp } from '../firebase/auth'
+import Layout from '../components/Layout';
 
 
 const SignUp = () => {
@@ -15,23 +16,25 @@ const SignUp = () => {
   }
 
   return (
-    <section className="signup">
-      <section>
-        <p>Sign Up</p>
+    <Layout>
+      <section className="sign">
+        <article>
+          <p>Sign Up</p>
+        </article>
+        <form className="sign__form" onSubmit={signUpForm}>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="signup_email" onChange={(e) => setEmail(e.target.value)} />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="signup_password" onChange={(e) => setPassword(e.target.value)} />
+
+          <label htmlFor="password_confirm">Confirm Password</label>
+          <input type="password" name="password_confirm" id="signup_password_confirm" onChange={(e) => setPasswordConfirm(e.target.value)} />
+
+          <button className="sign__btn" type="submit">Sign Up</button>
+        </form>
       </section>
-      <form className="signup__form" onSubmit={signUpForm}>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="signup_email" onChange={(e) => setEmail(e.target.value)} />
-
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="signup_password" onChange={(e) => setPassword(e.target.value)} />
-
-        <label htmlFor="password_confirm">Confirm Password</label>
-        <input type="password" name="password_confirm" id="signup_password_confirm" onChange={(e) => setPasswordConfirm(e.target.value)} />
-
-        <button className="signup__btn" type="submit">Sign Up</button>
-      </form>
-    </section>
+    </Layout>
   )
 }
 
