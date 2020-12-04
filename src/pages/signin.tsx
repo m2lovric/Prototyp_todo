2b16bfc7987d27662a0e3d358c351679255dea2f
Matteo <m2lovric@gmail.com>
2020-12-04 22:36:48 +0100

    Sign In page init

---
+++ src/pages/signin.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <>
+      <p>Sign In</p>
+      <form action="">
+        <label htmlFor="">Email</label>
+        <input type="text" />
+        <label htmlFor="">Password</label>
+        <input type="password" name="" id="" />
+        <button type="submit">Sign In</button>
+      </form>
+    </>
+  )
+}
+
+export default SignIn;
