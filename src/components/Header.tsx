d8ef83898b5256176be136dd2a152e1c5aacea24
Matteo <m2lovric@gmail.com>
2020-12-04 22:36:26 +0100

    Add Links to pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,15 @@ import React, { useState } from 'react'
 import { LogIn, SignUp } from './index'
 
 const Header = () => {
-  const [showLogIn, setShowLogIn] = useState(false);
-  const [showSignIn, setShowSignIn] = useState(false);
-
-  const handleCloseLog = () => setShowLogIn(false);
-  const handleShowLog = () => setShowLogIn(true);
-  const handleCloseSign = () => setShowSignIn(false);
-  const handleShowSign = () => setShowSignIn(true);
-
   return (
     <header>
       <section className="header container--xl">
         <Link to="/" className="header__logo">TaskManager</Link>
         <div className="header__sign">
-          <button className="link" onClick={handleShowLog}>LOGIN</button>
-          <button className="link" onClick={handleShowSign}>SIGNUP</button>
+          <Link to="/signin" className="link">Sign In</Link>
+          <Link to="/signup" className="link">Sign Up</Link>
         </div>
       </section>
-
-      <LogIn show={showLogIn} handleClose={handleCloseLog} />
-      <SignUp show={showSignIn} handleClose={handleCloseSign} />
     </header>
   )
 }
