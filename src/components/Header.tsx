f6ea80a2ae7fcc96154e617945c3de68251e830e
Matteo <m2lovric@gmail.com>
2020-12-06 00:40:19 +0100

    Add logout button and profile link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'gatsby'
-import React, { useState } from 'react'
-
-import { LogIn, SignUp } from './index'
+import React from 'react'
+import { useSelector} from 'react-redux'
+import { signOut } from '../firebase/auth'
 
 const Header = () => {
+  const user = useSelector((state: []) => state.user);
+
   return (
     <header>
       <section className="header container--xl">
@@ -11,6 +13,16 @@ const Header = () => {
         <div className="header__sign">
           <Link to="/signin" className="link">Sign In</Link>
           <Link to="/signup" className="link">Sign Up</Link>
+          {
+            user.uid
+            ? (
+              <>
+                <button onClick={() => signOut()}>Logout</button>
+                <Link to="/profile">Profile</Link>
+              </>
+            )
+            : ''
+          }
         </div>
       </section>
     </header>
