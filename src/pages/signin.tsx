267db704ab193181879fbdfbc7f59cae6ff734ef
Matteo <m2lovric@gmail.com>
2020-12-06 00:41:47 +0100

    Sign in page logic update

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout';
+import React, {useState} from 'react'
+import Layout from '../components/Layout'
+import { signIn } from '../firebase/auth'
+import { navigate } from 'gatsby'
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signInForm = (e: any) => {
+    e.preventDefault();
+    signIn(email, password);
+    navigate('/');
+  }
+
   return (
     <Layout>
       <section className="sign">
         <article>
           <p>Sign In</p>
         </article>
-        <form action="" className="sign__form">
-          <label htmlFor="">Email</label>
-          <input type="text" />
-          <label htmlFor="">Password</label>
-          <input type="password" name="" id="" />
+        <form className="sign__form" onSubmit={signInForm} >
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" onChange={(e) => setEmail(e.target.value)}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
           <button type="submit" className="sign__btn red">Sign In</button>
         </form>
       </section>
