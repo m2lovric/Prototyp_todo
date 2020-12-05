d92fb5b2c1cb82834a3f6f9f9a3e3fb510ce4d97
Matteo <m2lovric@gmail.com>
2020-12-06 00:39:05 +0100

    Add signIn, signOut methods

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,9 +1,14 @@
 import { auth } from './app'
 import { store } from '../../wrap-with-provider'
 import { userExist } from '../redux/actions'
+import { navigate } from 'gatsby'
 
 export const authState = auth.onAuthStateChanged((user) => {
-  store.dispatch(userExist(user));
+  if( user === null) {
+    store.dispatch(userExist({}));
+  } else {
+    store.dispatch(userExist(user));
+  }
   return user;
 })
 
@@ -17,4 +22,26 @@ export const signUp = (email: string, password: string, passwordConfirm: string)
         return err;
       })
   }
+}
+
+export const signIn = (email:string, password:string) =>{
+  auth.signInWithEmailAndPassword(email, password)
+    .then(data => {
+      return data;
+    })
+    .catch ((err: any) => {
+      navigate('/signin');
+      return err;
+    })
+}
+
+export const signOut = () => {
+  auth.signOut()
+  .then(() => {
+    console.log('User signed out');
+    navigate('/');
+  })
+  .catch ((err: any) => {
+    return err;
+  })
 }
