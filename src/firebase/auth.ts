174b5b4f72cc8ef51c3ef02cee514a88d30a322f
Matteo <m2lovric@gmail.com>
2020-12-05 23:51:24 +0100

    Track auth state and dispatch user

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,12 +1,19 @@
 import { auth } from './app'
+import { store } from '../../wrap-with-provider'
+import { userExist } from '../redux/actions'
+
+export const authState = auth.onAuthStateChanged((user) => {
+  store.dispatch(userExist(user));
+  return user;
+})
 
 export const signUp = (email: string, password: string, passwordConfirm: string) => {
   if (password === passwordConfirm) {
     auth.createUserWithEmailAndPassword(email, password)
-      .then((user: object) => {
-        return user;
+      .then((data: object) => {
+        return data;
       })
-      .catch((err: any) => {
+      .catch ((err: any) => {
         return err;
       })
   }
