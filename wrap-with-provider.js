1e4ea8479f90c3243ede9c0340d0595052e20c39
Matteo <m2lovric@gmail.com>
2020-12-05 23:40:56 +0100

    Combine reducers

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,16 +1,25 @@
 import React from "react"
 import { Provider } from "react-redux"
+import { combineReducers } from 'redux'
 
 import createStore from "./src/redux/store"
 
 import { taskReducer } from './src/redux/taskReducer'
-import { loadState, saveState } from './src/localStorage/localStorage'
+import { userReducer} from './src/redux/userReducer'
+
+import { saveState } from './src/localStorage/localStorage'
 
 export default ({ element }) => {
-  const persistedState = loadState();
-  const store = createStore(taskReducer, persistedState || [])
+
+  const allReducers = combineReducers({
+    task : taskReducer,
+    user : userReducer
+  });
+
+  const store = createStore(allReducers)
+
   store.subscribe(() => {
-    saveState(store.getState())
+    saveState(store.getState().task)
   })
   return <Provider store={store}>{element}</Provider>
 }
