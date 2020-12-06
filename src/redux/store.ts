4b24aaf9a5cd95ecffca6c23659f0c2d0fe531aa
Matteo <m2lovric@gmail.com>
2020-12-07 00:47:00 +0100

    Remove extension property

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { createStore as reduxCreateStore } from "redux"
 
-const createStore = (reducer: any, state: []) => reduxCreateStore(reducer, state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const createStore = (reducer: any, state: []) => reduxCreateStore(reducer, state)
 
 export default createStore
