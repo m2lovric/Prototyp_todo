e9e317c0334bd44035bc7acc9f6e67330fc6bc2f
Matteo <m2lovric@gmail.com>
2020-12-07 00:46:17 +0100

    Add extension property

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -16,7 +16,7 @@ const allReducers = combineReducers({
   user : userReducer
 });
 
-export const store = createStore(allReducers);
+export const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default ({ element }) => {
 
